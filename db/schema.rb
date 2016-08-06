f55cc418d93a514005a9fd2c33c0f2594a882ce4
Katsunori Kawaguchi <hamakata@gmail.com>
2016-08-06 17:30:15 +0900

    rails g scaffold Room name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806052906) do
+ActiveRecord::Schema.define(version: 20160806083004) do
 
   create_table "lounge_messages", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160806052906) do
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_lounge_messages_on_created_at"
     t.index ["user_id"], name: "index_lounge_messages_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
